a4fc24e2c6188c464d37ce4a7c766e1ae4a8bc80
SpringLee <1028789852@qq.com>
2017-03-24 13:34:52 +0800

    本地提交

---
+++ FluentScheduler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentScheduler;
+
+namespace FluentScheduler
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // 简单定时执行
+            //JobManager.AddJob(() =>  {     Console.WriteLine( DateTime.Now);    }, t =>  {     t.ToRunNow().AndEvery(1).Seconds();  });
+
+            //简单执行 方法
+            //JobManager.AddJob(Write,t => t.ToRunEvery(1).Seconds());
+
+            //每天10:00 定时执行
+            //JobManager.AddJob(Write,t => t.ToRunEvery(1).Days().At(10,34));
+            Console.ReadKey();
+        }
+
+        static void Write()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+       
+      
+    }
+}
---
+++ Timer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Timer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Set_thenout();
+         
+            Console.ReadKey();
+        }
+
+        static void Set_thenout()
+        {
+
+
+            System.Timers.Timer t = new System.Timers.Timer(1000);
+            var ss= new System.Timers.ElapsedEventHandler(theout);
+            t.Elapsed += ss;
+            t.AutoReset = true;
+            t.Start();
+          
+        }
+    
+        static void theout(object source, System.Timers.ElapsedEventArgs e)
+        {
+            if (e.SignalTime.Second%10==0)
+            {
+                Console.WriteLine("时间到了！");
+            }
+          
+        }
+    }
+}
